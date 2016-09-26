e1ecef85189ea251755228673300f014ada6ec28
ToniDias <toni.dias.91@gmail.com>
2016-09-26 15:19:24 +0200

    package

---
+++ src/web/HelloServlet.java
@@ -0,0 +1,17 @@
+package web;
+
+import java.io.IOException;
+
+/**
+ * Created by tonidias on 26.09.16.
+ */
+@javax.servlet.annotation.WebServlet(name = "HelloServlet")
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+}
